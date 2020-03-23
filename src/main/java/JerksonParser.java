97049ff24215c5ccaac63332b20331804217857f
Sandy Setiawan <ssetiawan@zipcodes-MBP-3.fios-router.home>
2020-03-23 15:01:30 -0400

    much still need to be done

--- src/main/java/JerksonParser.java
+++ src/main/java/JerksonParser.java
@@ -1,12 +1,14 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JerksonParser {
-    private String groceryData;
+    private String groceryList;
 
     public JerksonParser() {
-        this.groceryData = loadFile();
+        this.groceryList = loadFile();
     }
 
     private String loadFile() {
@@ -28,8 +30,43 @@ public class JerksonParser {
         return result.toString();
     }
 
-    public String getGroceryData(){
-        return groceryData;
+    public String getGroceryList() {
+        return groceryList;
     }
 
+    public String replaceBread(String str) throws Exception {
+        Pattern p = Pattern.compile("[Bb][Rr][Ee][Aa][Dd]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(str);
+        return m.replaceAll("Bread");
+    }
+
+    public String replaceMilk(String str) {
+        Pattern p = Pattern.compile("[Mm][Ii][Ll][Kk]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(str);
+        return m.replaceAll("Milk");
+    }
+
+    public String replaceCookies(String str) {
+        Pattern p = Pattern.compile("[Cc][Oo0][Oo0][Kk][Ii][Ee][Ss]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(str);
+        return m.replaceAll("Cookies");
+    }
+
+    public String replaceApples(String str) {
+        Pattern p = Pattern.compile("[Aa][Pp][Pp][Ll][Ee][Ss]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(str);
+        return m.replaceAll("Apples");
+    }
+
+    public String replaceName(String str) {
+        Pattern p = Pattern.compile("[Nn] [Aa] [Mm] [Ee]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(str);
+        return m.replaceAll("Name");
+    }
+
+    public String replacePrice(String str) {
+        Pattern p = Pattern.compile("[Pp] [Ri] [Cc] [Ee]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(str);
+        return m.replaceAll("Price");
+    }
 }
--- src/test/java/JerksonParserTest.java
+++ src/test/java/JerksonParserTest.java
@@ -1,2 +1,56 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class JerksonParserTest {
+    private String groceryText;
+    private JerksonParser jerksonParser;
+
+    @Before
+    public void setUp() {
+        this.jerksonParser = new JerksonParser();
+        this.groceryText = jerksonParser.getGroceryList();
+    }
+
+    @Test
+    public void replaceBread() {
+        String expected = groceryText;
+        String actual = jerksonParser.getGroceryList();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void replaceMilk() {
+        String expected = groceryText;
+        String actual = jerksonParser.getGroceryList();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void replaceCookies() {
+        String expected = groceryText;
+        String actual = jerksonParser.getGroceryList();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void replaceApples() {
+        String expected = groceryText;
+        String actual = jerksonParser.getGroceryList();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void replaceName() {
+        String expected = groceryText;
+        String actual = jerksonParser.getGroceryList();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void replacePrice() {
+        String expected = groceryText;
+        String actual = jerksonParser.getGroceryList();
+        Assert.assertEquals(expected, actual);
+    }
 }
